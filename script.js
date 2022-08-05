5d2b21afeb8d38531a4946e716627f3933882072
Coolestwaii <yougle55567@gmail.com>
2022-08-05 22:56:59 +0700

    done done

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const inputAdd = document.getElementById("input-add-todo");
 const todoCtn = document.getElementById("todo-container");
 
+empty = "Todo cannot be empty";
 inputAdd.onkeyup = (event) => {
-  if (event.key !== "Enter") return;
-
+  if (event.key !== "Enter") {
+    return;
+  } else if (inputAdd.value === "") {
+    alert("Todo cannot be empty");
+  }
   //your code here
+  else addTodo(inputAdd.value, false);
 };
 
 function addTodo(title, completed) {
@@ -28,6 +33,21 @@ function addTodo(title, completed) {
   deleteBtn.innerText = "Delete";
   deleteBtn.className = "btn btn-danger";
 
+  div.appendChild(span);
+  div.appendChild(doneBtn);
+  div.appendChild(deleteBtn);
+  todoCtn.appendChild(div);
+  div.onmouseover;
+
+  doneBtn.onclick = () => {
+    if (span.style.textDecoration !== "line-through") {
+      span.style.textDecoration = "line-through";
+      // const data = JSON.parse(localStorage.getItem("todo-container"));
+    } else span.style.textDecoration = "";
+  };
+  deleteBtn.onclick = () => {
+    div.remove();
+  };
   //your code here
   //append todo to HTML...
   //define buttons event...
